445c5a5b391555ac4cbf683a6c379bdb4a6de304
HamsteR674 <kirill@fomichiov.com>
2024-07-04 23:05:50 +0300

    Revert "Create db"

--- InfoCenter/Program.cs
+++ InfoCenter/Program.cs
@@ -1,10 +1,8 @@
 using InfoCenter;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
